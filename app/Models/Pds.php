13d1aa4b87074c3b84a0aa2bdbf9cedda7c922a0
ahasanul2000 <ahasanulhaque6505@gmail.com>
2024-10-09 18:33:55 +0600

    pds model and softDelete

---
+++ app/Models/Pds.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Pds extends Model
+{
+    use HasFactory;
+    protected $table = 'pds';
+
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['email', 'fullName', 'phone', 'address', 'age', 'image'];
+}
